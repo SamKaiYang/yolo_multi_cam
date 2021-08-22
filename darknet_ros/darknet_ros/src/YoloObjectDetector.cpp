65780dc7b4e8e534ec90952df2edfc35bf51c1fe
SamKaiYang <tt00621212@gmail.com>
2021-08-22 12:16:40 +0800

    Program function completed

--- darknet_ros/darknet_ros/src/YoloObjectDetector.cpp
+++ darknet_ros/darknet_ros/src/YoloObjectDetector.cpp
@@ -678,7 +678,7 @@ void *YoloObjectDetector::publishInThread()
       boundingBoxesResults_.header.frame_id = "detection";
       boundingBoxesResults_.image_header = headerBuff_[(buffIndex_ + 1) % 3];
       boundingBoxesResults_.cam_out = cam_number; //cam_out
-      boundingBoxesResults_.frame_id = imageHeader_.frame_id; //image->frame_id
+      boundingBoxesResults_.frame_id = headerBuff_[(buffIndex_ + 1) % 3].frame_id; //image->frame_id
       std::cout<<"Header:\n"<<imageHeader_.frame_id;
       boundingBoxesPublisher_.publish(boundingBoxesResults_);
     }
@@ -691,7 +691,9 @@ void *YoloObjectDetector::publishInThread()
     
     boundingBoxesResults_.header.frame_id = "No detection";
     boundingBoxesResults_.cam_out = cam_number; //cam_out
-    boundingBoxesResults_.frame_id = imageHeader_.frame_id; //image->frame_id
+    boundingBoxesResults_.frame_id = headerBuff_[(buffIndex_ + 1) % 3].frame_id; //image->frame_id
+    // boundingBoxesResults_.frame_id = imageHeader_.frame_id; //image->frame_id
+    boundingBoxesResults_.bounding_boxes.clear();
     boundingBoxesPublisher_.publish(boundingBoxesResults_);
   }
   if (isCheckingForObjects()) {
